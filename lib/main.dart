2a6a883750989c543016457f0fa75d2a7c7dac90
Jeremiah Opoku-Boateng <106244755+Tiptoplegend@users.noreply.github.com>
2025-01-16 22:48:46 +0000

    Created Welcome.dart page

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fintrack_app/Onboarding/Welcome.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,6 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: Welcomepage(),
     );
   }
 }
