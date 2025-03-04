4be82c55a596726344937341ab8f0e8a447e70e9
Jeremiah Opoku-Boateng <106244755+Tiptoplegend@users.noreply.github.com>
2025-03-04 22:51:15 +0000

    ...

--- lib/providers/SettingsScreen.dart
+++ lib/providers/SettingsScreen.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:fintrack_app/providers/theme_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class ThemeSettingsScreen extends StatelessWidget {
   const ThemeSettingsScreen({super.key});
