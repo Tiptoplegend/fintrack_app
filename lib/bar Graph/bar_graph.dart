e4992f1c4b1a7a1866d25dabe92cd3793d57e436
Jeremiah Opoku-Boateng <106244755+Tiptoplegend@users.noreply.github.com>
2025-03-05 14:50:27 +0000

    Add fl_chart dependency and update Analytics screen with expense summary

---
+++ lib/bar Graph/bar_graph.dart
@@ -0,0 +1,116 @@
+import 'package:fintrack_app/bar%20Graph/bar_data.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class MyBarGraph extends StatelessWidget {
+  final double? maxY;
+  final double sunAmount;
+  final double monAmount;
+  final double tueAmount;
+  final double wedAmount;
+  final double thuAmount;
+  final double friAmount;
+  final double satAmount;
+
+  const MyBarGraph({
+    super.key,
+    required this.maxY,
+    required this.sunAmount,
+    required this.monAmount,
+    required this.tueAmount,
+    required this.wedAmount,
+    required this.thuAmount,
+    required this.friAmount,
+    required this.satAmount,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    BarData myBarData = BarData(
+      sunAmount: sunAmount,
+      monAmount: monAmount,
+      tueAmount: tueAmount,
+      wedAmount: wedAmount,
+      thuAmount: thuAmount,
+      friAmount: friAmount,
+      satAmount: satAmount,
+    );
+
+    myBarData.initializedBarData();
+
+    return BarChart(BarChartData(
+      maxY: maxY,
+      minY: 0,
+      gridData: FlGridData(show: false),
+      borderData: FlBorderData(show: false),
+      titlesData: FlTitlesData(
+        show: true,
+        topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+        rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+        leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+        bottomTitles: AxisTitles(
+          sideTitles: SideTitles(
+            showTitles: true,
+            getTitlesWidget: getBottomTitles,
+          ),
+        ),
+      ),
+      barGroups: myBarData.barData
+          .map((data) => BarChartGroupData(x: data.x, barRods: [
+                BarChartRodData(
+                    toY: data.y,
+                    width: 25,
+                    borderRadius: BorderRadius.circular(4),
+                    color: Colors.green,
+                    backDrawRodData: BackgroundBarChartRodData(
+                      show: true,
+                      toY: maxY,
+                      color: Colors.grey[100],
+                    ))
+              ]))
+          .toList(),
+    ));
+  }
+}
+
+Widget getBottomTitles(double value, TitleMeta meta) {
+  const style = TextStyle(
+    color: Colors.grey,
+    fontWeight: FontWeight.bold,
+    fontSize: 14,
+  );
+  Widget text;
+  switch (value.toInt()) {
+    case 0:
+      text = const Text('Sun', style: style);
+      break;
+    case 1:
+      text = const Text('Mon', style: style);
+      break;
+    case 2:
+      text = const Text('Tue', style: style);
+      break;
+    case 3:
+      text = const Text('Wed', style: style);
+      break;
+    case 4:
+      text = const Text('Thu', style: style);
+      break;
+    case 5:
+      text = const Text('Fri', style: style);
+      break;
+    case 6:
+      text = const Text('Sat', style: style);
+    default:
+      text = const Text(
+        '',
+        style: style,
+      );
+      break;
+  }
+
+  return SideTitleWidget(
+    meta: meta,
+    child: text,
+  );
+}
